eab14d070d40a387bc36c3a731a79de2be63c7ca
ueren97 <ueren.hack@gmail.com>
2020-03-08 15:41:42 +0900

    transrate to English

--- sound_and_response.py
+++ sound_and_response.py
@@ -1,6 +1,7 @@
 # coding:utf8
 """
-取得した音声ファイルをテキスト化して、Dialogflowにテキストを渡すプログラムに渡す。
+exchange posted audio file into text file 
+-> export file which export it to dialogflow 
 """
 import base64
 
@@ -9,20 +10,20 @@ import httplib2
 
 from dialogflow_api import access_to_dialogflow
 
-# 音声からテキストにするプログラム
-# APIキーを設定
-KEY = "AIzaSyCYdoExbu90ejput_e6mzyudQ7XgweQJos"
+# Speech to Text
+# set the api key
+KEY = "**********"
 
-# 音声認識に使うファイル名
-speech_file = "weather.wav"
+# filename
+speech_file = "**********"
 
-# URL情報
-DISCOVERY_URL = "https://speech.googleapis.com/$discovery/rest"
+# URL information
+DISCOVERY_URL = "https://speech.googleapis.com/**********"
 
 
 def get_speech_service():
     """
-    APIの情報を返す関数
+    return information of api
     """
     http = httplib2.Http()
     return discovery.build(
@@ -32,33 +33,36 @@ def get_speech_service():
 
 def send_text_to_dialogflow():
     """
-    録音した音声をテキスト化して、Dialogflowにテキストを渡すプログラムに渡す。
+    exchange recorded audio to text 
+    -> export to function which export text to dialogflow 
     """
-    # 音声ファイルを開く
+    # open audio file
     with open(speech_file, 'rb') as speech:
         speech_content = base64.b64encode(speech.read())
 
-    # APIの情報を取得して、音声認識を行う
+    # get api information -> speech to text
     service = get_speech_service()
     service_request = service.speech().recognize(
         body={
             'config': {
                 'encoding': 'LINEAR16',
                 'sampleRateHertz': 48000,
-                'languageCode': 'ja-JP',   # 日本語に設定
+                'languageCode': 'ja-JP',   # set Japanese
                 'enableWordTimeOffsets': 'false',
             },
             'audio': {
                 'content': speech_content.decode('utf-8')
-                }
-            })
+            }
+        })
 
-    response = service_request.execute()  # SpeechAPIによる認識結果を保存
-    # わかりやすいようにコンソールに認識された音声を出力
+    response = service_request.execute()  # save response esult
+
+    # check output result
     print(response['results'][0]["alternatives"][0]["transcript"])
 
-    # テキストをDialogflowに渡してレスポンスを受け取る
-    access_to_dialogflow(response["results"][0]["alternatives"][0]["transcript"])
+    # export the text to dialogflow -> get result
+    access_to_dialogflow(response["results"][0]
+                         ["alternatives"][0]["transcript"])
 
 
 if __name__ == '__main__':
